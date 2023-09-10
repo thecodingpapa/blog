f03f7b75cbc48696b78edce59d3e588ce94cf70c
Victorer <rainishere20@gmail.com>
2023-09-10 21:42:38 +0900

    testing to vercel

--- components/header.tsx
+++ components/header.tsx
@@ -5,8 +5,7 @@ const Header = () => {
   return (
     <div className="tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
       <Link href="/" className="hover:underline">
-      <Image src={"https://res.cloudinary.com/dqrkrqm9y/image/upload//c_thumb,w_46,g_auto/v1694303495/back-button_kac1dl.png"} width={24} height={24} className="w-9 h-9 rounded-full mr-4" alt={''} />
-
+      <Image src={"/back-button_kac1dl.png"} width={24} height={24} className="w-9 h-9 rounded-full mr-4" alt={''} />
       </Link>
       </div>
   )
