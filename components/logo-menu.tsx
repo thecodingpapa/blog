b7c35307595054b3830a3aaa6c6bd096a72e5ee2
Victorer <rainishere20@gmail.com>
2023-09-11 14:02:34 +0900

    edit menu more like teachable

--- components/logo-menu.tsx
+++ components/logo-menu.tsx
@@ -4,8 +4,8 @@ import React from 'react';
 
 const LogoMenuComponent = ({}) => {
   return (
-    <div className='fixed top-0 w-screen bg-black'>
-      <div className="container mx-auto px-5">
+    <div className='fixed top-0 w-screen bg-black flex justify-center'>
+      <div style={{width:"960px"}} className="px-5">
           <div className="flex justify-between items-center p-4">
             <div className="flex flex-row flex-shrink-0 items-baseline">
               {/* <Image src="/thecodingpapa_logo.png" width={48} height={48} alt="Logo" className="h-12" /> */}
