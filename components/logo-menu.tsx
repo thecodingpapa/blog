adade58ff7b87332a908fe165fbbbbb7f3023fb1
Victorer <rainishere20@gmail.com>
2023-09-11 13:51:30 +0900

    change menubar color

--- components/logo-menu.tsx
+++ components/logo-menu.tsx
@@ -4,17 +4,17 @@ import React from 'react';
 
 const LogoMenuComponent = ({}) => {
   return (
-    <div className='fixed top-0 w-screen bg-white border-y-black border-y'>
+    <div className='fixed top-0 w-screen bg-black'>
       <div className="container mx-auto px-5">
           <div className="flex justify-between items-center p-4">
             <div className="flex flex-row flex-shrink-0 items-baseline">
               {/* <Image src="/thecodingpapa_logo.png" width={48} height={48} alt="Logo" className="h-12" /> */}
-              <h1 className="text-5xl font-bold">Blog</h1>
-              <p className="text-sg ml-1">by 더코딩파파</p>
+              <h1 className="text-xl text-white font-bold">Blog</h1>
+              <p className="text-sm text-white ml-1">by 더코딩파파</p>
             </div>
             <div className="flex gap-4">
               <button
-                className="bg-transparent hover:bg-black text-black-700 font-semibold hover:text-white py-2 px-4 border border-black-500 hover:border-black bg-black rounded"
+                className="bg-transparent hover:text-cyan text-white text-sm"
                 onClick={() => {
                   window.location.href = 'https://www.thecodingpapa.com';
                 }}
@@ -22,7 +22,7 @@ const LogoMenuComponent = ({}) => {
                   코딩 캠퍼스
               </button>
               <button
-                className="bg-transparent hover:bg-black text-black-700 font-semibold hover:text-white py-2 px-4 border border-black-500 hover:border-black bg-black rounded"
+                className="bg-transparent hover:text-cyan text-white text-sm"
                 onClick={() => {
                   window.location.href = 'https://blog.thecodingpapa.com';
                 }}
